7d92f56a339f32e5f5e61e313f33238e22b615ce
Marlon <dracomarmol@gmail.com>
2021-08-17 09:43:56 -0600

    Target worflow

---
+++ R/procomer_country.R
@@ -0,0 +1,103 @@
+procomer_country <- function(url, download_path_var) {
+
+  ## folder
+  eCaps <- list(
+    chromeOptions =
+      list(prefs = list(
+        "download.default_directory" = download_path_var
+      ))
+  )
+
+  ## Version of chrome driver
+  vers <- binman::list_versions("chromedriver") %>%
+    unname() %>%
+    unlist()
+  drivern <- length(vers) - 1
+
+  ## set driver
+  driver <- RSelenium::rsDriver(
+    chromever = vers[drivern],
+    port = 4562L, extraCapabilities = eCaps
+  )
+
+  ## set client
+  remote_driver <- driver[["client"]]
+  remote_driver$open()
+  remote_driver$navigate(url)
+
+  ## clicks
+  ### Manual 6
+  address_element <-
+    remote_driver$findElement(
+      using = "id",
+      value = "ASPxRoundPanel2_RBValorST"
+    )
+  address_element$clickElement()
+  Sys.sleep(3)
+
+
+  ### Region
+  address_element <-
+    remote_driver$findElement(
+      using = "id",
+      value = "ASPxRoundPanel2_CBRegion"
+    )
+  address_element$clickElement()
+  Sys.sleep(3)
+  ### Pais
+  address_element2 <-
+    remote_driver$findElement(
+      using = "id",
+      value = "ASPxRoundPanel2_CBPais_S_D"
+    )
+  address_element2$clickElement()
+  Sys.sleep(2)
+
+
+  ### Selección de años
+  #### casilla de años
+  address_element <-
+    remote_driver$findElement(
+      using = "id",
+      value = "ASPxPivotGrid1_sortedpgHeader5F"
+    )
+  address_element$clickElement()
+  Sys.sleep(2)
+  #### todos los años
+  address_element <-
+    remote_driver$findElement(
+      using = "id",
+      value = "ASPxPivotGrid1FTRIAll"
+    )
+  address_element$clickElement()
+  Sys.sleep(2)
+  #### aplicar cambios
+  address_element <-
+    remote_driver$findElement(
+      using = "id",
+      value = "ASPxPivotGrid1_FPWOK_B"
+    )
+  address_element$clickElement()
+  Sys.sleep(3)
+
+  ### descarga
+  address_element <-
+    remote_driver$findElement(
+      using = "id",
+      value = "ASPxRoundPanel3_ImageButton3"
+    )
+  address_element$clickElement()
+  Sys.sleep(2)
+
+  ### removemos
+  remote_driver$close()
+  driver$client$quit()
+  rm(driver)
+  gc()
+  system("taskkill /im java.exe /f", intern = FALSE, ignore.stdout = FALSE)
+
+  #### path
+  path_return <- paste0(download_path, "\\", "PivotGrid.xls")
+
+  return(path_return)
+}
--- _targets.R
+++ _targets.R
@@ -1,17 +1,27 @@
 # _targets.R
 library(targets)
 library(tarchetypes)
-source("R/functions.R")
+library(here)
+sapply(list.files(here("R"), full.names = T), source)
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("tidyverse",
                             "RSelenium",
                             "here",
                             "httr",
-                            "xlm2"
+                            "xml2",
                             "XML")) # add packages here
-
-tar_pipeline(
-
-  # add targets here
-
+# params 
+download_path <- normalizePath(here("temp"))
+# flow
+list(
+  ## enlace
+  tar_target(
+    exports_link,
+    "http://sistemas.procomer.go.cr/estadisticas/inicio.aspx"
+  ),
+  ## temporal data by country
+  tar_target(
+    temp_country,
+    procomer_country(exports_link, download_path)
+  )
 )
