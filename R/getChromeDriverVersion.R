4561389cb2b8d4def7be22337075e926463df055
Marlon <dracomarmol@gmail.com>
2021-08-21 15:00:35 -0600

    Update getChromeDriverVersion.R

--- R/getChromeDriverVersion.R
+++ R/getChromeDriverVersion.R
@@ -63,6 +63,7 @@ driver_number <- function(port = 4567L, force = FALSE, verbose = FALSE) {
     rD
   }
   versions <- binman::list_versions("chromedriver")
+  print(versions)
   if (is.list(versions) & length(versions) == 0) {
     versions <- getChromeDriverVersion()
     print(versions)
