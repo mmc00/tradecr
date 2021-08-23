b94dd85f2ec150156b22ec37b2895710659af054
mmc00 <dracomarmol@gmail.com>
2021-08-23 16:08:30 -0600

    Create prueba2.py

---
+++ python/prueba2.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from selenium import webdriver
+import time
+import os
+
+download_path = os.getenv('TEMP_PATH')
+driver_path = os.getenv('DRIVER_PATH')
+# download_path = "D:\\tradecr\\temp"
+# driver_path = "D:\chromedriver_win32\chromedriver.exe"
+
+options = webdriver.ChromeOptions()
+prefs = {"download.default_directory" : download_path}
+options.add_experimental_option("prefs",prefs)
+
+browser = webdriver.Chrome(driver_path, options=options)
