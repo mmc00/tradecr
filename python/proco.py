ee6be2b16bc07e2a56b2b0631bf3ce8bad13f5b5
mmc00 <dracomarmol@gmail.com>
2021-08-23 15:26:30 -0600

    Update proco.py

--- python/proco.py
+++ python/proco.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 import time
 import os
 
-# download_path = os.getenv('TEMP_PATH')
-# driver_path = os.getenv('DRIVER_PATH')
-download_path = "D:\\tradecr\\temp"
-driver_path = "D:\chromedriver_win32\chromedriver.exe"
+download_path = os.getenv('TEMP_PATH')
+driver_path = os.getenv('DRIVER_PATH')
+# download_path = "D:\\tradecr\\temp"
+# driver_path = "D:\chromedriver_win32\chromedriver.exe"
 
 options = webdriver.ChromeOptions()
 prefs = {"download.default_directory" : download_path}
