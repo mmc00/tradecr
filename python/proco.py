5d08880f9c008445991a0ff6da1d0271a9342dae
mmc00 <dracomarmol@gmail.com>
2021-08-23 17:47:40 -0600

    Update proco.py

--- python/proco.py
+++ python/proco.py
@@ -6,3 +6,8 @@ import os
 download_path = os.getenv('TEMP_PATH')
 driver_path = os.getenv('DRIVER_PATH')
 print(os.getenv('TEMP_PATH'))
+print(os.getenv('DRIVER_PATH'))
+
+options = webdriver.ChromeOptions()
+prefs = {"download.default_directory" : download_path}
+options.add_experimental_option("prefs",prefs)
