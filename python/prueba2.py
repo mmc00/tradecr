4526f8717cd377a784f5c47546bb4126da887b76
mmc00 <dracomarmol@gmail.com>
2021-08-23 16:20:56 -0600

    Update prueba2.py

--- python/prueba2.py
+++ python/prueba2.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 import time
 import os
 
-download_path = os.getenv('TEMP_PATH')
-driver_path = os.getenv('DRIVER_PATH')
+download_path = os.environ['TEMP_PATH']
+driver_path = os.environ['DRIVER_PATH']
 # download_path = "D:\\tradecr\\temp"
 # driver_path = "D:\chromedriver_win32\chromedriver.exe"
 options = webdriver.ChromeOptions()
