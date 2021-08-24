6c656bf53e1f3b7d05902aee3a5717c779876444
mmc00 <dracomarmol@gmail.com>
2021-08-23 19:00:35 -0600

    Update proco.py

--- python/proco.py
+++ python/proco.py
@@ -13,6 +13,13 @@ options = webdriver.ChromeOptions()
 prefs = {"download.default_directory" : download_path}
 options.add_experimental_option("prefs",prefs)
 options.add_argument("--remote-debugging-port=9222")
+options.add_argument('--no-sandbox')
+options.add_argument('--window-size=1420,1080')
+options.add_argument('--headless')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
+options.add_argument("--disable-notifications")
+options.add_experimental_option('useAutomationExtension', False)
 browser = webdriver.Chrome(driver_path, options=options)
 
 # start browsing
