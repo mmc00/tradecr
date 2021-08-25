a901243a89e5d17165027ab910e4ae380a15bce6
Marlon <dracomarmol@gmail.com>
2021-08-24 21:05:02 -0600

    Creating targets flow righ

--- R/functions.R
+++ R/functions.R
@@ -1,49 +1,98 @@
-# functions.R
-# activar sharepoint
-activate_sharepoint_site <- function(page_url) {
-  ## Se usa el paquete "Microsoft365R
-  ## si es la primera vez que se usa
-  ## se loguea mediante get_sharepoint_site()
-  ## si da error hay que solicitar permiso TI (Alfonso)
-  ## si se cambio de contraseña se utiliza el comando
-  ## AzureAuth::clean_token_directory()
-  site <- get_sharepoint_site(site_url = page_url)
-  # obtenemos folder
-  drv <- site$get_drive()
+long_country_data <- function(path) {
+  data <- read_excel(path) %>%
+    rename("country" = "...1") %>%
+    # fixing uk
+    mutate(
+      country =
+        if_else((country == "Reino Unido-No UE" | country == "Reino Unido-UE"),
+          "Reino Unido", country
+        )
+    ) %>%
+    # removing total
+    filter(country != "Grand Total") %>%
+    select(country, any_of(paste0(1990:2100))) %>%
+    slice(-c(1:2)) %>%
+    pivot_longer(-country, names_to = "year") %>%
+    group_by(country, year) %>%
+    summarise(value = sum(value, na.rm = T), .groups = "drop") %>% 
+    mutate(time = as.POSIXlt(Sys.time()))
+
+  return(data)
 }
-# obtener el etag (para ver si el archivo cambia)
-get_etag <- function(site = server_call, path_sharepoint = obj_download) {
-  call_site <- site$get_item_properties(path_sharepoint)
-  call_site$eTag
+
+long_chapter_data <- function(path) {
+  data <- read_excel(path) %>%
+    rename("chapter" = "...1") %>%
+    filter(chapter != "Grand Total") %>%
+    select(chapter, any_of(paste0(1990:2100))) %>%
+    slice(-1) %>%
+    pivot_longer(-chapter, names_to = "year") %>%
+    group_by(chapter, year) %>%
+    summarise(value = sum(value, na.rm = T), .groups = "drop") %>% 
+    mutate(time = as.POSIXlt(Sys.time()))
+  
+  return(data)
 }
-# descargar archivos del sharepoint
-download_xls <- function(site, path_sharepoint, path_destiny,
-                         etag = 0) {
-  print(etag)
-  site$download_file(path_sharepoint,
-    dest = path_destiny,
-    overwrite = T
-  )
-  path_destiny
+
+append_data <- function(data, name){
+  relative_path <- here("data", name)
+  if (file.exists(relative_path)) {
+    write.table(data, relative_path, append = T, row.names = F)
+  } else{
+    write.csv(data, relative_path, row.names = F)
+  }
+  return(relative_path)
 }
-## leer diccionario
-reading_dict <- function(path) {
-  read_excel(path,
-    sheet = "CodNombr",
-    .name_repair = "minimal"
+
+reading_old_data <- function(path, dummy){
+  read.csv(path)
+}
+
+last_old_data <- function(data, dummy){
+  id_date <- data %>%
+    mutate(time = as.POSIXlt(time)) %>% 
+    mutate(
+      id_year = year(time),
+      id_month = month(time),
+      id_day = day(time),
+      id_hour = hour(time),
+      id_min = minute(time),
+      id_sec = second(time)
+    ) %>%
+    arrange(
+      id_year, id_month, id_day,
+      id_hour, id_min, id_sec
+    ) %>%
+    slice(1) %>%
+    pull(time)
+  
+  data <- data %>% 
+    filter(time %in% id_date) %>% 
+    group_by(year) %>% 
+    summarise(value_old = sum(value, na.rm = T), .groups = "drop")
+  
+  return(data)
+}
+
+last_new_data <- function(data){
+   data <- data %>% 
+    group_by(year) %>% 
+    summarise(value = sum(value, na.rm = T), .groups = "drop")
+   
+   return(data)
+}
+
+comparing_data <- function(new, old, tol = 0.0001) {
+  data <- left_join(new %>%
+    mutate(year = as.numeric(year)),
+  old %>%
+    mutate(year = as.numeric(year)),
+  by = "year"
   ) %>%
-    select(1:2) %>%
-    set_names(c("code_pais2", "nombre")) %>%
-    mutate(code_pais2 = as.numeric(code_pais2))
-}
-## descargar efectiva del diccionario
-effective_download <- function(server, url, path, etag) {
-  download_xls(
-    server,
-    url,
-    path_destiny = path,
-    etag = etag
-  )
-  return(path)
+    mutate(check = abs(value - value_old) <= tol) %>%
+    filter(!check)
+
+  write.csv(data, here("data", "check_procomer.csv"), row.names = F)
+  return(here("data", "check_procomer.csv"))
 }
 
--- _targets.R
+++ _targets.R
@@ -6,61 +6,85 @@ sapply(list.files(here("R"), full.names = T), source)
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c(
   "tidyverse",
-  "RSelenium",
   "here",
   "httr",
   "xml2",
   "XML",
   "readxl",
-  "binman",
-  "Microsoft365R"
-),
-debug = "chrome_version") # add packages here
+  "lubridate"
+))
+# debug = "chrome_version") # add packages here
 # params
 download_path <- normalizePath(here("temp"))
 temp_path <- here("temp")
 dir.create(temp_path, showWarnings = F)
 # flow
 list(
-  # enlace para Exportaciones
+  # path for files
   tar_target(
-    exports_link,
-    "http://sistemas.procomer.go.cr/estadisticas/inicio.aspx"
+    temp.condata.path,
+    list.files(here("temp")) %>%
+      str_subset(., "con_data") %>%
+      here("temp", .),
+    format = "file"
   ),
-  ## getting chrome version
   tar_target(
-    chrome_version,
-    driver_number(force = T),
-    cue = tar_cue_force(TRUE)
+    temp.capdata.path,
+    list.files(here("temp")) %>%
+      str_subset(., "cap_data") %>%
+      here("temp", .),
+    format = "file"
   ),
-  ## temporal data by country
+  # reshape
   tar_target(
-    temp_country,
-    procomer_country(
-      exports_link, download_path,
-      chrome_version
+    temp.condata.long,
+    long_country_data(temp.condata.path)
+  ),
+  tar_target(
+    temp.capdata.long,
+    long_chapter_data(temp.capdata.path)
+  ),
+  # adding to dataset
+  tar_target(
+    appending.condata,
+    append_data(
+      temp.condata.long,
+      "historical_country_data_procomer.csv"
     ),
-    cue = tar_cue_force(TRUE)
+    format = "file"
   ),
-  ## clening temp folder
   tar_target(
-    cleaning_temp,
-    file.remove(list.files(temp_path, full.names = T)),
-    cue = tar_cue_force(TRUE)
+    appending.capdata,
+    append_data(
+      temp.capdata.long,
+      "historical_chapter_data_procomer.csv"
+    ),
+    format = "file"
+  ),
+  # reading last data
+  tar_target(
+    old.data,
+    reading_old_data(
+      here(
+        "data",
+        "historical_chapter_data_procomer.csv"
+      ),
+      appending.capdata
+    )
+  ),
+  tar_target(
+    sort.old.data,
+    last_old_data(old.data, appending.capdata)
+  ),
+  tar_target(
+    sort.new.data,
+    last_new_data(temp.capdata.long)
+  ),
+  tar_target(
+    compare.agg.data,
+    comparing_data(
+      sort.new.data,
+      sort.old.data
+    )
   )
-  # ## download the file
-  # tar_target(
-  #   file_dict,
-  #   effective_download(
-  #     server_call,
-  #     dict_link,
-  #     "input/dict_sicomex.xlsx",
-  #     dict_etag
-  #   )
-  # ),
-  # ## reading dict data
-  # tar_target(
-  #   sicomex_dict,
-  #   reading_dict(file_dict)
-  # )
 )
