e542092cb6e03cee59c4cf5c1f0f0ae2b46a1488
Marlon <dracomarmol@gmail.com>
2021-08-27 18:02:23 -0600

    Adding bccr

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,11 @@
+getting_temps <- function(folder, name, dummy) {
+  list.files(folder) %>%
+    str_subset(., name) %>%
+    paste0(folder, "/", .)
+}
+
 long_country_data <- function(patho, dummy) {
   
-  date_dummy <- as.character(as.POSIXct(Sys.time()))
   data <- read_excel(path = patho) %>%
     rename("country" = "...1") %>%
     # fixing uk
@@ -17,7 +22,7 @@ long_country_data <- function(patho, dummy) {
     pivot_longer(-country, names_to = "year") %>%
     group_by(country, year) %>%
     summarise(value = sum(value, na.rm = T), .groups = "drop") %>% 
-    mutate(time = date_dummy)
+    mutate(time = now())
 
   return(data)
 }
@@ -32,12 +37,12 @@ long_chapter_data <- function(path) {
     pivot_longer(-chapter, names_to = "year") %>%
     group_by(chapter, year) %>%
     summarise(value = sum(value, na.rm = T), .groups = "drop") %>% 
-    mutate(time = Sys.time())
+    mutate(time = now())
   
   return(data)
 }
 
-append_data <- function(data, name){
+append_data <- function(data, name, dummy = NULL){
   
   relative_path <- paste0("data", "/", name)
   print(file.exists(relative_path))
@@ -55,7 +60,7 @@ reading_old_data <- function(path, dummy){
 
 last_old_data <- function(data, dummy){
   id_date <- data %>%
-    # mutate(time = as.POSIXlt(time)) %>% 
+    mutate(time = lubridate::as_date(time)) %>%
     mutate(
       id_year = year(time),
       id_month = month(time),
@@ -79,6 +84,32 @@ last_old_data <- function(data, dummy){
   return(data)
 }
 
+last_old_data_month <- function(data, dummy){
+  id_date <- data %>%
+    mutate(time = lubridate::as_date(time)) %>%
+    mutate(
+      id_year = year(time),
+      id_month = month(time),
+      id_day = day(time),
+      id_hour = hour(time),
+      id_min = minute(time),
+      id_sec = second(time)
+    ) %>%
+    arrange(
+      id_year, id_month, id_day,
+      id_hour, id_min, id_sec
+    ) %>%
+    slice(1) %>%
+    pull(time)
+  
+  data <- data %>% 
+    filter(time %in% id_date) %>% 
+    group_by(year, month) %>% 
+    summarise(value_old = sum(value, na.rm = T), .groups = "drop")
+  
+  return(data)
+}
+
 last_new_data <- function(data){
    data <- data %>% 
     group_by(year) %>% 
@@ -87,8 +118,16 @@ last_new_data <- function(data){
    return(data)
 }
 
+last_new_data_month <- function(data) {
+  data <- data %>%
+    group_by(year, month) %>%
+    summarise(value = sum(value, na.rm = T), .groups = "drop")
+
+  return(data)
+}
+
 comparing_data <- function(new, old, tol = 0.0001) {
-  data <- left_join(new %>%
+  data <- full_join(new %>%
     mutate(year = as.numeric(year)),
   old %>%
     mutate(year = as.numeric(year)),
@@ -101,3 +140,63 @@ comparing_data <- function(new, old, tol = 0.0001) {
   return("data/check_procomer.csv")
 }
 
+comparing_data_month <- function(new, old, tol = 0.0001) {
+  data <- full_join(new %>%
+    mutate(year = as.numeric(year)) %>%
+    mutate(month = as.numeric(month)),
+  old %>%
+    mutate(year = as.numeric(year)) %>%
+    mutate(month = as.numeric(month)),
+  by = c("year", "month")
+  ) %>%
+    mutate(check = abs(value - value_old) <= tol) %>%
+    filter(!check)
+
+  write.csv(data, "data/check_bccr.csv", row.names = F)
+  return("data/check_bccr.csv")
+}
+
+bccr_imp0_api <- function(user, password,
+                          start_date, end_date) {
+  base <- "https://gee.bccr.fi.cr/Indicadores/Suscripciones/WS/wsindicadoreseconomicos.asmx/ObtenerIndicadoresEconomicos?"
+  qr <- list(
+    Indicador = "1993",
+    FechaInicio = start_date,
+    FechaFinal = end_date,
+    Nombre = "Marlon",
+    Subniveles = "S",
+    CorreoElectronico = user,
+    Token = password
+  )
+  res <- GET(base, query = qr)
+  dat <- as_list(read_xml(res))
+
+  dat2 <- as_tibble(dat) %>%
+    unnest_wider(DataSet) %>%
+    unnest(cols = names(.)) %>%
+    unnest_wider(Datos_de_INGC011_CAT_INDICADORECONOMIC) %>%
+    select(-element) %>%
+    filter(NUM_VALOR != "NULL") %>%
+    map(unlist) %>%
+    bind_cols() %>%
+    mutate(DES_FECHA = substr(DES_FECHA, 1, 10)) %>%
+    mutate(DES_FECHA = as.Date(DES_FECHA)) %>%
+    mutate(NUM_VALOR = as.numeric(NUM_VALOR)) %>%
+    mutate(id_month = lubridate::month(DES_FECHA)) %>%
+    mutate(id_year = lubridate::year(DES_FECHA))
+  
+  return(dat2)
+}
+
+getting_agg_imports <- function(data, study_month,
+                                study_year) {
+  new_data <- data %>%
+    filter(id_month == study_month | id_year == study_year) %>%
+    select(-DES_FECHA, -COD_INDICADORINTERNO) %>%
+    rename(
+      "value" = "NUM_VALOR",
+      "year" = "id_year",
+      "month" = "id_month"
+    ) %>%
+    mutate(time = now())
+}
--- python/telegram_message.py
+++ python/telegram_message.py
@@ -4,6 +4,7 @@ import os
 
 # reading temp file
 file_path = os.getenv('CHECK_PATH')
+file_path_imp = os.getenv('CHECK_PATH')
 api_key = os.getenv('ITRADECRAPI')
 user_id = os.getenv('USER_CALL')
 
@@ -11,10 +12,22 @@ with open(file_path) as fp:
     reader = csv.reader(fp, delimiter=",", quotechar='"')
     data_read = [row for row in reader]
 
+with open(file_path_imp) as fp:
+    reader_imp = csv.reader(fp, delimiter=",", quotechar='"')
+    data_read_imp = [row for row in reader_imp]    
+
 # set up bot 
 bot = telegram.Bot(token=api_key)
 # send message if file has more than one row  
 if(len(data_read) > 1):
-  bot.send_message(chat_id=user_id, text= data_read)
+  text0 = ("There is a problem on Exports" + "/n" + data_read)
+  bot.send_message(chat_id=user_id, text=text0)
 else:
-  bot.send_message(chat_id=user_id, text="Everything its right!")
+  bot.send_message(chat_id=user_id, text = "Everything its right with Exports!")
+
+# send message if file has more than one row  
+if(len(data_read_imp) > 1):
+  text1 = ("There is a problem on Imports" + "/n" + data_read_imp)
+  bot.send_message(chat_id=user_id, text=text1)
+else:
+  bot.send_message(chat_id=user_id, text="Everything its right with Imports!")
