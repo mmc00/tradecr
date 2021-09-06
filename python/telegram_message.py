002be05a57686a7d6dc0bcf48514ddd13e42cad4
Marlon <dracomarmol@gmail.com>
2021-09-06 15:28:33 -0600

    adding kim

--- python/telegram_message.py
+++ python/telegram_message.py
@@ -8,6 +8,9 @@ file_path = os.getenv('CHECK_PATH')
 file_path_imp = os.getenv('CHECK_PATH')
 api_key = os.getenv('ITRADECRAPI')
 user_id = os.getenv('USER_CALL')
+user_id2 = os.getenv('USER_CALL2')
+
+user_ids = [user_id, user_id2]
 
 with open(file_path) as fp:
     reader = csv.reader(fp, delimiter=",", quotechar='"')
@@ -23,16 +26,20 @@ data_read_imp_t = tabulate(data_read_imp)
 bot = telegram.Bot(token=api_key)
 # send message if file has more than one row  
 if(len(data_read) > 1):
-  text0 = ("There is a problem on Exports")
-  bot.send_message(chat_id=user_id, text=text0)
-  bot.send_message(chat_id=user_id, text=data_read_t)
+  for chat in user_ids:
+    text0 = ("There is a problem on Exports")
+    bot.send_message(chat_id=chat, text=text0)
+    bot.send_message(chat_id=chat, text=data_read_t)
 else:
-  bot.send_message(chat_id=user_id, text = "Everything its right with Exports!")
+  for chat in user_ids:
+    bot.send_message(chat_id=chat, text = "Everything is fine with Exports!")
 
 # send message if file has more than one row  
 if(len(data_read_imp) > 1):
-  text1 = ("There is a problem on Imports")
-  bot.send_message(chat_id=user_id, text=text1)
-  bot.send_message(chat_id=user_id, text=data_read_imp_t)
+  for chat in user_ids:
+    text1 = ("There is a problem on Imports")
+    bot.send_message(chat_id=chat, text=text1)
+    bot.send_message(chat_id=chat, text=data_read_imp_t)
 else:
-  bot.send_message(chat_id=user_id, text="Everything its right with Imports!")
+  for chat in user_ids:
+    bot.send_message(chat_id=chat, text="Everything is fine with Imports!")
